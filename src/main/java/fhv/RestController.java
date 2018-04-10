30cd438d808222845e719a94c1735227ca8b47ad
i7Gamer <timorzipa@gmail.com>
2018-04-10 12:50:00 +0200

    critical path nearly finished, only missing late start & finish

--- src/main/java/fhv/RestController.java
+++ src/main/java/fhv/RestController.java
@@ -1,7 +1,10 @@
 package fhv;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,14 +20,12 @@ public class RestController {
     @RequestMapping(value = "/criticalPath", method = RequestMethod.POST)
     @ResponseBody
     public List<Node> getCP(@RequestBody Node[] nodes) {
-
-
         return criticalPathService.getCP(Arrays.asList(nodes));
     }
 
-    @RequestMapping(value = "/pert", method = RequestMethod.GET)
+    @RequestMapping(value = "/pert", method = RequestMethod.POST)
     @ResponseBody
-    public List<Node> getPERT(@RequestParam(name = "cells") List<Node> nodes) {
-        return pertService.getPERT(nodes);
+    public List<Node> getPERT(@RequestBody Node[] nodes) {
+        return pertService.getPERT(Arrays.asList(nodes));
     }
 }
