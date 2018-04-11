36ab7578a589a653a5f8137acfaf62a04f88cd5c
i7Gamer <timorzipa@gmail.com>
2018-04-11 17:51:31 +0200

    pert finished

--- src/main/java/fhv/PertService.java
+++ src/main/java/fhv/PertService.java
@@ -3,11 +3,22 @@ package fhv;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PertService {
+
+    List<Node> elements;
+
     public List<Node> getPERT(List<Node> nodes) {
-        // TODO work
-        return nodes;
+        elements = nodes.stream().filter(n -> !n.getType().equals("link")).collect(Collectors.toList());
+
+        for (Node n : elements) {
+            if (n.getEstTime() != 0 && n.getMinTime() != 0 && n.getMaxTime() != 0) {
+                n.setDuration((n.getMinTime() + 4 * n.getEstTime() + n.getMaxTime()) / 6);
+            }
+        }
+
+        return elements;
     }
 }
